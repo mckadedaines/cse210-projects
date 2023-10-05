ce63fefe63f56f6296d4007dc8c0e53aff802307
Mckade Daines <mckadedaines1@gmail.com>
2023-10-05 14:11:32 -0700

    new prompt question added

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -11,9 +11,9 @@ public class Journal
     private string[] prompts = {
         "Who was the most interesting person I interacted with today?",
         "What was the best part of my day?",
-        "How did I see the hand of the Lord in my life today?",
         "What was the strongest emotion I felt today?",
-        "If I had one thing I could do over today, what would it be?"
+        "If I had one thing I could do over today, what would it be?",
+        "Do you feel like you got closer to your goals today? Why?"
     };
 
     public Journal()
